7f20e8658992b6071aac24b8a64b9c302a9d265b
Voronovich-interexy <voronovichinterexy@gmail.com>
2023-02-01 16:37:56 +0300

    feat: responsive design, media queries

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const navbar = document.querySelector('.navigation');
-const checkbox = document.querySelector('#menu__toggle');
+// const navbar = document.querySelector('.navigation');
+// const checkbox = document.querySelector('#menu__toggle');
 
-window.onload = () => {
-  checkbox.checked = false;
-};
+// window.onload = () => {
+//   checkbox.checked = false;
+// };
 
-checkbox.onchange = () => {
-  checkbox.checked ? (navbar.style.width = '20%') : (navbar.style.width = '10%');
-};
+// checkbox.onchange = () => {
+//   checkbox.checked ? (navbar.style.width = '20%') : (navbar.style.width = '10%');
+// };
