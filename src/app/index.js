6b259edf63709a6b0000d30ce58e7010fe5b7825
Voronovich-interexy <voronovichinterexy@gmail.com>
2023-02-06 21:03:18 +0300

    feat: implement animation via timestamp and requesAnimationFrame

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,57 @@
 import './styles/main.scss';
+
+window.onload = () => {
+  const container = document.querySelector('.square__container-js');
+  const maxXPosition = container.getBoundingClientRect().width;
+  timeoutAnimate(maxXPosition);
+  frameAnimate(maxXPosition);
+};
+
+function timeoutAnimate(maxXPosition) {
+  const refreshRate = 10;
+  let block = document.querySelector('.running__square-js');
+  let speedX = 6;
+  let positionX = 0;
+
+  window.setInterval(() => {
+    positionX = positionX + speedX;
+    if (positionX > maxXPosition - 40 || positionX < 0) {
+      speedX = speedX * -1;
+    }
+    block.style.transform = `translateX(${positionX}px)`;
+  }, refreshRate);
+}
+
+// === дано
+// скорость
+// фреймрейт
+// timestamp
+// ширина артикла
+// ширина квадрата
+
+function frameAnimate(maxXPosition) {
+  let block = document.querySelector('.running__square-frame');
+  let positionX = 0;
+
+  let speedX = 10;
+  let time;
+
+  function step() {
+    window.requestAnimationFrame(step);
+    let now = new Date().getTime();
+    let dt = now - (time || now);
+
+    time = now;
+
+    positionX += speedX * ((dt / 1000) * 60);
+    console.log((dt / 1000) * 60);
+
+    block.style.transform = `translateX(${positionX}px)`;
+
+    if (positionX > maxXPosition - 40 || positionX < 0) {
+      speedX = speedX * -1;
+    }
+  }
+
+  window.requestAnimationFrame(step);
+}
--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,7 @@ module.exports = {
       title: 'Markup',
       filename: 'index.html',
       template: 'src/app/template.html',
+      inject: 'body',
     }),
   ],
 };
