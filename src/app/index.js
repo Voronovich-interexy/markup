b97867e211d45be0c16518cb117ba9c390278a07
Voronovich-interexy <voronovichinterexy@gmail.com>
2023-02-07 10:16:14 +0300

    fix: revrite animation according to the recomendation of the mentor

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,24 @@
 import './styles/main.scss';
 
+// === дано
+// скорость
+// фреймрейт
+// timestamp
+// ширина артикла
+// ширина квадрата
+
 window.onload = () => {
   const container = document.querySelector('.square__container-js');
   const maxXPosition = container.getBoundingClientRect().width;
-  timeoutAnimate(maxXPosition);
-  frameAnimate(maxXPosition);
+  const timeBlock = document.querySelector('.running__square-js');
+  const frameBlock = document.querySelector('.running__square-frame');
+  timeoutAnimate(maxXPosition, timeBlock);
+  frameAnimate(maxXPosition, frameBlock);
 };
 
-function timeoutAnimate(maxXPosition) {
+function timeoutAnimate(maxXPosition, block) {
   const refreshRate = 10;
-  let block = document.querySelector('.running__square-js');
+
   let speedX = 6;
   let positionX = 0;
 
@@ -22,27 +31,47 @@ function timeoutAnimate(maxXPosition) {
   }, refreshRate);
 }
 
-// === дано
-// скорость
-// фреймрейт
-// timestamp
-// ширина артикла
-// ширина квадрата
+// function frameAnimate(maxXPosition,block) {
+//    let positionX = 0;
 
-function frameAnimate(maxXPosition) {
-  let block = document.querySelector('.running__square-frame');
+//   let speedX = 10;
+//   let time;
+
+//   function step(ts) {
+//     window.requestAnimationFrame(step);
+//     let dt = ts - (time || ts);
+
+//     time = ts;
+
+//     positionX += speedX * ((dt / 1000) * 60);
+
+//     block.style.transform = `translateX(${positionX}px)`;
+
+//     if (positionX > maxXPosition - 40 || positionX < 0) {
+//       speedX = speedX * -1;
+//     }
+//   }
+
+//   window.requestAnimationFrame(step);
+// }
+
+function frameAnimate(maxXPosition, block) {
   let positionX = 0;
-
+  let framerate = 60;
   let speedX = 10;
-  let time;
+  let prevTime = 0;
 
-  function step(ts) {
+  function step(time) {
+    if (!prevTime) {
+      prevTime = time;
+    }
+    let timestamp = time - prevTime;
+    //  console.log('t:', timestamp);
+
     window.requestAnimationFrame(step);
-    let dt = ts - (time || ts);
 
-    time = ts;
-
-    positionX += speedX * ((dt / 1000) * 60);
+    prevTime = time;
+    positionX += speedX * ((timestamp / 1000) * framerate);
 
     block.style.transform = `translateX(${positionX}px)`;
 
