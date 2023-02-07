65b4391ae25baf255b6dc609807906a1d00463d0
Voronovich-interexy <voronovichinterexy@gmail.com>
2023-02-07 22:22:54 +0300

    feat: implement search

--- src/app/index.js
+++ src/app/index.js
@@ -10,7 +10,6 @@ import './styles/main.scss';
 window.onload = () => {
   const container = document.querySelector('.square__container-js');
   const maxXPosition = parseInt(getComputedStyle(container).width);
-  console.log(maxXPosition);
   const timeBlock = document.querySelector('.running__square-js');
   const frameBlock = document.querySelector('.running__square-frame');
   timeoutAnimate(maxXPosition, timeBlock);
@@ -25,36 +24,16 @@ function timeoutAnimate(maxXPosition, block) {
 
   window.setInterval(() => {
     positionX = positionX + speedX;
-    if (positionX > maxXPosition - 40 || positionX < 0) {
+    if (positionX > maxXPosition - 40) {
+      positionX = maxXPosition - 40;
       speedX = speedX * -1;
+    } else if (positionX < 0) {
+      speedX = speedX * -1;
+      positionX = 0;
     }
     block.style.transform = `translateX(${positionX}px)`;
   }, refreshRate);
 }
-
-// function frameAnimate(maxXPosition,block) {
-//    let positionX = 0;
-
-//   let speedX = 10;
-//   let time;
-
-//   function step(ts) {
-//     window.requestAnimationFrame(step);
-//     let dt = ts - (time || ts);
-
-//     time = ts;
-
-//     positionX += speedX * ((dt / 1000) * 60);
-
-//     block.style.transform = `translateX(${positionX}px)`;
-
-//     if (positionX > maxXPosition - 40 || positionX < 0) {
-//       speedX = speedX * -1;
-//     }
-//   }
-
-//   window.requestAnimationFrame(step);
-// }
 
 function frameAnimate(maxXPosition, block) {
   let positionX = 0;
@@ -70,8 +49,8 @@ function frameAnimate(maxXPosition, block) {
     prevTime = time;
     positionX += speedX * (timestamp / 1000);
 
-    if (positionX > maxXPosition) {
-      positionX = maxXPosition;
+    if (positionX > maxXPosition - 40) {
+      positionX = maxXPosition - 40;
       speedX = speedX * -1;
     } else if (positionX < 0) {
       speedX = speedX * -1;
@@ -85,3 +64,168 @@ function frameAnimate(maxXPosition, block) {
   let ss = window.requestAnimationFrame(step);
   window.blur(() => cancelAnimationFrame(ss));
 }
+
+// =========== SELECT virtualized list ==========
+
+const dataToSearch = [
+  'erlv',
+  'nszmovdos',
+  'tryx',
+  'eyg',
+  'fmobclpwb',
+  'yyghxvm',
+  'kgeqdfsmm',
+  'cdjyqwhnzum',
+  'cwkvltnuv',
+  'lhpczzclkzir',
+  'plqbarnpj',
+  'lgqsj',
+  'lnczthfr',
+  'qyoqeuisq',
+  'oadaaad',
+  'gnfrkxeufzob',
+  'kadmmze',
+  'amwmk',
+  'acbzyrwr',
+  'jkim',
+  'dfxeorwjrs',
+  'rikwtfyg',
+  'donjpsq',
+  'mwxdxurgjdja',
+  'xux',
+  'lkp',
+  'hxafsnteik',
+  'ldcuhwoofgs',
+  'xhtk',
+  'tmz',
+  'szghtmyxc',
+  'uwftlchgyatm',
+  'bjxsykgc',
+  'qwdnofr',
+  'nsxige',
+  'rzxrzptpg',
+  'rwn',
+  'fkjoy',
+  'gwwimhjwwn',
+  'vvlrhms',
+  'mpdpieqey',
+  'srimvzziaup',
+  'dprvgisqnz',
+  'ssc',
+  'skhmeoto',
+  'wdaphbi',
+  'zfa',
+  'vfxwprelr',
+  'spgpbnyomt',
+  'dhhzdmopi',
+  'znpoe',
+  'lrtwsydtnfg',
+  'zpcdeusbinho',
+  'iklj',
+  'rwntzcqpxiiz',
+  'dneggq',
+  'pokqvnk',
+  'vaygn',
+  'lcmqxw',
+  'xebquevuaw',
+  'ciztrwo',
+  'xibt',
+  'bxdarvwnzs',
+  'dpqq',
+  'xcvvg',
+  'hemwxlyk',
+  'gxqm',
+  'szmoxrtnh',
+  'ngtoaswvl',
+  'pycqgbhdpjq',
+  'rrzso',
+  'upgzjdgcqbc',
+  'vhnroxbypd',
+  'sjqjgkclbko',
+  'chzir',
+  'qozuakwq',
+  'tdu',
+  'ckgntrexbo',
+  'zteii',
+  'znh',
+  'zyrfbyrekkke',
+  'kbaisgobrq',
+  'xam',
+  'obeqxj',
+  'ovddxte',
+  'lzxssakbbna',
+  'gqsdqhj',
+  'jdqqkd',
+  'vjqqhw',
+  'hne',
+  'rivvxst',
+  'kaekfghhdmd',
+  'lkeyzof',
+  'lulwvfwhmd',
+  'bcpdikkose',
+  'ddzxe',
+  'epbffhhp',
+  'zffkdbqr',
+  'zmfojgov',
+  'law',
+  'aqwfz',
+  'qwe',
+  'qwer',
+  'qweqwr',
+  'qwedc',
+  'qwedcq',
+  'qwedcqx',
+  'qwedxga',
+  'qwertxga',
+  'qwertaxga',
+  'qwertaasdxga',
+  'qwehhzzxga',
+  'qwehh',
+  'qwehhalla',
+  'qwehhalas',
+  'qwehhalasasda',
+];
+
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+const searchValues = (searchText) => {
+  let matches = dataToSearch.filter((data) => {
+    const regex = new RegExp(`^${searchText}`, 'gi');
+    return data.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matchList.innerHTML = '';
+    matches = [];
+  }
+
+  outputHtml(matches.slice(0, 10));
+};
+
+const deleteInfo = (searchText) => {
+  if (matchList.firstChild && matchList.firstChild.textContent === 'no matches') {
+    matchList.removeChild(matchList.firstChild);
+  } else {
+    return;
+  }
+};
+
+const createLi = (text) => {
+  let li = document.createElement('li');
+  li.innerText = text;
+  return li;
+};
+
+const outputHtml = (matches) => {
+  if (matches.length > 0) {
+    matchList.innerHTML = '';
+    matches.map((match) => matchList.append(createLi(match)));
+  } else {
+    matchList.innerHTML = '';
+    matchList.append(createLi('no matches'));
+  }
+};
+
+search.addEventListener('input', () => searchValues(search.value));
+search.addEventListener('blur', () => deleteInfo(search.value));
